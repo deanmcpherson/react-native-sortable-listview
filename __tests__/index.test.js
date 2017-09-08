4bd04e509480135195d006e90e0358481f2febf9
Neo <nihgwu@live.com>
2017-09-08 23:05:00 +0800

    fix layout measure, close #101

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,6 +5,8 @@ import MyComponent from '../example';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('InteractionManager');
+
 test('renders correctly', () => {
   const tree = renderer.create(
     <MyComponent />
