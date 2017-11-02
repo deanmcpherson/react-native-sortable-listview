6e2a5beee754163f50d967a53fda42b5d9f8d991
Alexandre Nicolas <Kornflexx@users.noreply.github.com>
2017-11-02 17:49:46 +0100

    Fix forgotten function call

--- index.js
+++ index.js
@@ -450,7 +450,7 @@ class SortableListView extends React.Component {
 
   componentDidMount() {
     InteractionManager.runAfterInteractions(() => {
-      this.timer = setTimeout(() => this && this.measureWrapper, 0)
+      this.timer = setTimeout(() => this && this.measureWrapper(), 0)
     })
   }
 
